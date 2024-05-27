23999dffe3c3d930c4e76bf81cff29387fe2716a
Harish Kunchala <32921476+khkred@users.noreply.github.com>
2024-05-27 14:36:31 -0700

    Basic useState() completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learn_flutter_hooks/counter_page.dart';
 
 import 'login_page.dart';
 
@@ -18,6 +19,17 @@ class HomePage extends StatelessWidget {
               'Home Page',
             ),
             const SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const CounterPage(),
+                      ),
+                    ),
+                child: const Text('Go to Counter Page')),
+
+            const SizedBox(height: 20),
+            //This button takes us to a login page that just uses StatefulWidget
             ElevatedButton(
               onPressed: () {
                 Navigator.push(
